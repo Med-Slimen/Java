0de658a811f236ba372a976ca20892a92f7973b0
Med-Slimen <mohamedamineslimene01@gmail.com>
2025-10-03 23:23:42 +0100

    tp3'

--- POO/tp3'/Point.java
+++ POO/tp3'/Point.java
@@ -21,10 +21,16 @@ public class Point {
      public String getNom(){
         return this.nom;
      }
+     public void setAbs(int abs){
+      this.abs=abs;
+     }
+     public void setOrd(int ord){
+      this.ord=ord;
+     }
      public void affiche(){
         System.out.println(this.nom+"("+this.abs+","+this.ord+")");
      }
      public double distance(Point p){
-        return Math.sqrt(Math.pow(this.abs-p.abs,2)+Math.pow(this.ord-p.ord,2));
+        return Math.sqrt(Math.pow(this.abs - p.abs, 2) + Math.pow(this.ord - p.ord, 2));
      }
 }
